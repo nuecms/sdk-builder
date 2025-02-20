d66c8c3e39877bcd51afffb38b006ee938610c1f
Terry Cai <gidcai@gmail.com>
2025-02-20 16:46:53 +0800

    fix: ts error

--- src/transformers/jsonTransformer.ts
+++ src/transformers/jsonTransformer.ts
@@ -1,6 +1,6 @@
 import { ResponseTransformer } from './responseTransformer';
 
-export const jsonTransformer: ResponseTransformer = (data: any) => {
+export const jsonTransformer: ResponseTransformer<any> = (data: any, options: any, response: any) => {
   if (data && typeof data === 'object') {
     data.transformed = true;
   }
